98eeb54cc582ee989e94e6a6bd8827a759037f48
Alan Timbó <alancorreiatimbo@gmail.com>
2023-04-18 16:56:15 -0300

    fix: joi schema change for program release

--- src/schemas/payments-schema.ts
+++ src/schemas/payments-schema.ts
@@ -4,7 +4,7 @@ const cardDataSchema = Joi.object({
     issuer: Joi.string().required(),
     number: Joi.number().required(),
     name: Joi.string().required(),
-    expirationDate: Joi.date().required(),
+    expirationDate: Joi.string().required(),
     cvv: Joi.number().required()
 })
 
