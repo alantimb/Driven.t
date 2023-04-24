67a3840455c403be3e625e0ead2190cc29c39484
Alan Timbó <alancorreiatimbo@gmail.com>
2023-04-24 01:06:38 -0300

    refactor: remove function 'next'

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -3,19 +3,19 @@ import hotelsService from "@/services/hotels-service";
 import { NextFunction, Response } from "express";
 import httpStatus from "http-status";
 
-export async function getAllHotels(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+export async function getAllHotels(req: AuthenticatedRequest, res: Response) {
    const { userId } = req;
-   
+      
    try {
       const hotels = await hotelsService.getAllHotels(userId);
 
       return res.status(httpStatus.OK).send(hotels);
    } catch (err) {
-      next(err);
+      return res.sendStatus(httpStatus.NOT_FOUND)
    }
 }
 
-export async function getAllRoomsByHotelId(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+export async function getAllRoomsByHotelId(req: AuthenticatedRequest, res: Response) {
    const hotelId  = +req.params.hotelId;
    const { userId } = req;
 
@@ -24,6 +24,6 @@ export async function getAllRoomsByHotelId(req: AuthenticatedRequest, res: Respo
   
       return res.status(httpStatus.OK).send(hotelRooms);
    } catch (err) {
-      next(err);
+      return res.sendStatus(httpStatus.NOT_FOUND)
    } 
 }
